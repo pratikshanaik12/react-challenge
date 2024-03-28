0965eab1d3989335fe4526c677ef3d91997cd980
pratikshanaik12 <41194108+pratikshanaik12@users.noreply.github.com>
2024-03-27 22:54:58 -0400

    Data Fetch Challenge

---
+++ react-challenge/src/App.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import List from './List';
+import Form from './Form';
+import Table from './Table';
+function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+
+  const [reqType, setReqType] = useState('users');
+  const [items, setItems] = useState([])
+ 
+  useEffect(()=>{
+    const fetchItems = async()=>{
+      try{
+        const response = await fetch(`${API_URL}${reqType}`)
+        if (!response.ok) throw Error(`Did not get expected data`)
+        const data = await response.json()
+        
+      setItems(data);
+      } catch(err){
+        console.log(err)
+      }
+    }
+    fetchItems()
+  }, [reqType])
+
+  return (
+    <div className="App">
+      <Form 
+        reqType={reqType}
+        setReqType={setReqType}
+      />
+
+      {/* <List items = {items}/> */}
+
+      <Table
+        items = {items}
+      />
+      
+    </div>
+  );
+}
+
+export default App;
